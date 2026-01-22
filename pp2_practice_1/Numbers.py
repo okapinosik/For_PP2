b8fa7ce0e6bd3ee6d8bffe279a71c13db54e3fc4
okapinosik <ernur20080107@gmail.com>
2026-01-22 20:50:37 +0500

    Fourth Example

---
+++ pp2_practice_1/Numbers.py
@@ -0,0 +1,23 @@
+x = 1    
+y = 2.8  
+z = 1j   
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x1 = 1
+y1 = 35656222554887711
+z1 = -3255522
+
+print(type(x1))
+print(type(y1))
+print(type(z1))
+
+x2 = 35e3
+y2 = 12E4
+z2 = -87.7e100
+
+print(type(x2))
+print(type(y2))
+print(type(z2))
