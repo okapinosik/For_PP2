91fb3819e0d7f80eb64becd3b1822589ce5922fd
okapinosik <ernur20080107@gmail.com>
2026-01-22 20:49:41 +0500

    Third Example

---
+++ pp2_practice_1/Output.py
@@ -0,0 +1,21 @@
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")
+print('This will also work!')
+
+# print(This will cause an error) <- error
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+#sub theme numbers
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
