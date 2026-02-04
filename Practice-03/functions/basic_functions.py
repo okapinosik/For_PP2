12c945f7cc4ca7b6f2c374e5928f4fced8243016
okapinosik <ernur20080107@gmail.com>
2026-02-04 20:50:39 +0500

    1 example\

---
+++ Practice-03/functions/basic_functions.py
@@ -0,0 +1,15 @@
+def my_function():
+  print("Hello from a function")
+  
+def my_function():
+  print("Hello from a function")
+
+my_function()
+
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
